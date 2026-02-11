74f13796b73ac4c4f8fea85a85e7d5ab1552fa85
hoanvu20041125@gmail.com <hoanvu20041125@gmail.com>
2026-02-11 21:25:12 +0700

    Create Folder Structure

---
+++ BE/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({ message: "Game Backend Running " });
+});
+
+module.exports = app;
